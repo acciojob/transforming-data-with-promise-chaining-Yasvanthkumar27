56aa03ee8a06e04fc69244fdd0e00f4138e19b7f
Yasvanth Kumar <yasvanthkumar48@gmail.com>
2025-12-01 15:41:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+const ip = document.getElementById("ip");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+btn.onclick = function () {
+  const value = parseFloat(ip.value);
+  output.textContent = "";
+
+  new Promise((resolve) => {
+    setTimeout(() => {
+      output.textContent = "Result: " + value;
+      resolve(value);
+    }, 2000);
+  })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const res = num * 2;
+          output.textContent = "Result: " + res;
+          resolve(res);
+        }, 2000);
+      });
+    })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const res = num - 3;
+          output.textContent = "Result: " + res;
+          resolve(res);
+        }, 1000);
+      });
+    })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const res = num / 2;
+          output.textContent = "Result: " + res;
+          resolve(res);
+        }, 1000);
+      });
+    })
+    .then((num) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const res = num + 10;
+          output.textContent = "Final Result: " + res;
+          resolve(res);
+        }, 1000);
+      });
+    });
+};
